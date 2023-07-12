0663b330b94235d28325fae3d270544db280c803
Eddy Gomez <contact@eddyg.dev>
2023-07-11 21:34:55 -0400

    connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require(`dotenv`).config();
 
 const express = require(`express`);
+const mongoose = require(`mongoose`);
 const workoutRoutes = require("./routes/workouts");
 
 //express app
@@ -17,7 +18,15 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/workouts", workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
-});
+// connect to db
+mongoose
+  .connect("mongodb+srv://netNinja:2yM2pYmxmGDjqxj6@shibbydata.xqkzcnr.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`connected to db listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
